9d1829c8ba3442d37a55db80a270330d30d31844
STarrant <stephenvtarrant@gmail.com>
2021-11-02 20:10:48 +0000

    Minor:  retry render question list

--- app.py
+++ app.py
@@ -198,10 +198,10 @@ def add_room():
 # Render survey questions list page
 @app.route("/survey_question_list")
 def survey_question_list():
-    # questions = list(mongo.db.surveyQuestions.find())
+    questions = list(mongo.db.surveyQuestions.find())
     # questions = list(mongo.db.surveyQuestions.find().sort("_id", 1))
     # rooms = list(mongo.db.electricalRooms.find())
-    return render_template("survey-question-list.html")
+    return render_template("survey-question-list.html", questions=questions)
 
 
 # Render user list
